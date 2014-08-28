b8793ce7c76f168c11e899e1e56d2dd031fff8b6
Herman Schaaf <hermanschaaf@gmail.com>
2014-08-28 20:55:56 +0900

    Refactor and add start of Lists endpoints

--- bestsellers.go
+++ bestsellers.go
@@ -1,22 +1,49 @@
+// Package bestsellers provides a simplified interface to the New York
+// Times Best Sellers List API.
 package bestsellers
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
+	"time"
 )
 
+// dateFmt describes the date format as specified by the API (YYYY-MM-DD)
+const dateFmt = "2006-01-02"
+
+// BaseResponse is the basic response returned by all endpoints in the API.
+// It includes the status of the request, a copyright notice and the number
+// of results returned for the query.
+type BaseResponse struct {
+	Status     string `json:"status"`
+	Copyright  string `json:"copyright"`
+	NumResults int    `json:"num_results"`
+}
+
+// Client is the main struct used to interface with the API.
+// API methods are implemented as methods on this struct, and so
+// the first step of any interaction with the API client must be
+// to insantiate this struct. This can be done using the NewClient
+// function.
 type Client struct {
 	apiKey  string
 	rootURL string
 }
 
+// NewClient returns a new Client, which can be used to interface
+// with the API. It takes only an APIKey string as single argument.
 func NewClient(APIKey string) *Client {
 	rootURL := "http://api.nytimes.com/"
 	return &Client{APIKey, rootURL}
 }
 
+// getURL takes a resource path as a string and returns a
+// url.URL customized for the client settings and specified
+// path on the API root.
 func (c *Client) getURL(path string) (*url.URL, error) {
 	u, err := url.Parse(c.rootURL)
 	if err != nil {
@@ -31,13 +58,44 @@ func (c *Client) getURL(path string) (*url.URL, error) {
 	return u, err
 }
 
-type ListNamesResponse struct {
-	Status     string            `json:"status"`
-	Copyright  string            `json:"copyright"`
-	NumResults int               `json:"num_results"`
-	Results    []listNamesResult `json:"results"`
+// get takes a path string and performs a GET request to the specified
+// path for this client, and returns the result as a byte slice, or an
+// not-nil error if something went wrong during the request.
+func (c *Client) get(path string, offset int) ([]byte, error) {
+	u, err := c.getURL(path)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	// add offset parameter if set
+	if offset > 0 {
+		params := u.Query()
+		params.Add("offset", strconv.Itoa(offset))
+		u.RawQuery = params.Encode()
+	}
+
+	response, err := http.Get(u.String())
+	if err != nil {
+		return []byte{}, err
+	}
+	defer response.Body.Close()
+
+	contents, err := ioutil.ReadAll(response.Body)
+	return contents, err
 }
 
+// ListNamesResponse describes the response returned by ListNames.
+// It includes meta-information returned by the API, such as status
+// and copyright information, as well as the number of results returned.
+type ListNamesResponse struct {
+	BaseResponse
+	Results []listNamesResult `json:"results"`
+}
+
+// listNamesResult describes the form of a single ListName result returned
+// by ListNames. DisplayName contains a human-formatted description of the list name,
+// and ListNameEncoded is the API-friendly name that should be used when calling
+// other API methods, such as Lists and ListsByDate.
 type listNamesResult struct {
 	ListName            string `json:"list_name"`
 	DisplayName         string `json:"display_name"`
@@ -49,19 +107,38 @@ type listNamesResult struct {
 
 // ListNames returns the response for /svc/books/v2/lists/names
 func (c *Client) ListNames() (*ListNamesResponse, error) {
-	u, err := c.getURL("svc/books/v2/lists/names")
+	content, err := c.get("svc/books/v2/lists/names", 0)
 	if err != nil {
 		return nil, err
 	}
-	response, err := http.Get(u.String())
-	if err != nil {
-		return nil, err
-	}
-	defer response.Body.Close()
-
-	contents, err := ioutil.ReadAll(response.Body)
 
 	var listNames ListNamesResponse
-	err = json.Unmarshal(contents, &listNames)
+	err = json.Unmarshal(content, &listNames)
 	return &listNames, err
+}
+
+type ListsResponse struct {
+}
+
+func (c *Client) lists(path string, offset int) (*ListsResponse, error) {
+	content, err := c.get(path, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	var lists ListsResponse
+	err = json.Unmarshal(content, &lists)
+	return &lists, err
+}
+
+// Lists returns the reponse for /svc/books/v2/lists/{list-name}.
+func (c *Client) Lists(listName string, offset int) (*ListsResponse, error) {
+	p := fmt.Sprintf("/svc/books/v2/lists/%s")
+	return c.lists(p, offset)
+}
+
+// ListsByDate returns the reponse for /svc/books/v2/lists/{date}/{list-name}.
+func (c *Client) ListsByDate(listName string, date time.Time, offset int) (*ListsResponse, error) {
+	p := fmt.Sprintf("/svc/books/v2/lists/%s/%s", date.Format(dateFmt), listName)
+	return c.lists(p, offset)
 }
